112f0c7aff6fa16a587383789226005e5af0a564
ArrayLabs <chris@arraylabs.com>
2019-07-07 14:27:35 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -20,8 +20,10 @@ class PixelRingSkill(MycroftSkill):
 
     def initialize(self):
         LOG.debug("initialising")
+        
+        pixel_ring = find()
 
-        #pixel_ring.set_brightness(10)
+        pixel_ring.set_brightness(10)
 
         self.add_event('recognizer_loop:record_begin',
                        self.handle_listener_wakeup)
