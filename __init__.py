6515282f3013baa8bb52f9674a5401e663fec1d6
ArrayLabs <chris@arraylabs.com>
2019-07-07 15:54:45 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,5 +1,21 @@
-# TODO: Add an appropriate license to your skill before publishing.  See
-# the LICENSE file for more information.
+# Copyright 2019, arraylabs
+# Github https://github.com/arraylabs
+#
+# Inspired by https://github.com/andlo/picroft-google-aiy-voicekit-skill.git
+# Inspired by https://github.com/domcross/respeaker-io-skill
+# Inspired by https://github.com/j1nx/respeaker-4mic-hat-skill
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from pixel_ring import pixel_ring
 from adapt.intent import IntentBuilder
