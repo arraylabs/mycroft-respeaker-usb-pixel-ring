fd50dec0ef81f5567b9a7ef34b0a8eb58caa6204
arraylabs <chris@arraylabs.com>
2019-07-07 13:17:24 -0400

    fix init

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 # TODO: Add an appropriate license to your skill before publishing.  See
 # the LICENSE file for more information.
 
+import time
+from pixel_ring import pixel_ring
 from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill, intent_handler
 from mycroft.util.log import LOG
@@ -15,9 +17,6 @@ class PixelRingSkill(MycroftSkill):
     def __init__(self):
         super(PixelRingSkill, self).__init__(name="PixelRingSkill")
 
-        # Initialize working variables used within the skill.
-        self.count = 0
-
     # The "handle_xxxx_intent" function is triggered by Mycroft when the
     # skill's intent is matched.  The intent is defined by the IntentBuilder()
     # pieces, and is triggered when the user's utterance matches the pattern
@@ -29,29 +28,21 @@ class PixelRingSkill(MycroftSkill):
     #   'Hello world'
     #   'Howdy you great big world'
     #   'Greetings planet earth'
-    @intent_handler(IntentBuilder("").require("Hello").require("World"))
-    def handle_hello_world_intent(self, message):
+    @intent_handler(IntentBuilder("").require("Enable").require("Pixel"))
+    def handle_enable_pixel_ring_intent(self, message):
         # In this case, respond by simply speaking a canned response.
         # Mycroft will randomly speak one of the lines from the file
         #    dialogs/en-us/hello.world.dialog
-        self.speak_dialog("hello.world")
+        self.speak_dialog("EnablePixelRing")
 
-    @intent_handler(IntentBuilder("").require("Count").require("Dir"))
-    def handle_count_intent(self, message):
-        if message.data["Dir"] == "up":
-            self.count += 1
-        else:  # assume "down"
-            self.count -= 1
-        self.speak_dialog("count.is.now", data={"count": self.count})
-
-    # The "stop" method defines what Mycroft does when told to stop during
-    # the skill's execution. In this case, since the skill's functionality
-    # is extremely simple, there is no need to override it.  If you DO
-    # need to implement stop, you should return True to indicate you handled
-    # it.
-    #
-    # def stop(self):
-    #    return False
+    @intent_handler(IntentBuilder("").require("Disable").require("Pixel"))
+    def handle_disable_pixel_ring_intent(self, message):
+        self.speak_dialog("DisablePixelRing")
+        # if message.data["Dir"] == "up":
+        #     self.count += 1
+        # else:  # assume "down"
+        #     self.count -= 1
+        # self.speak_dialog("count.is.now", data={"count": self.count})
 
 # The "create_skill()" method is used to create an instance of the skill.
 # Note that it's outside the class itself.
